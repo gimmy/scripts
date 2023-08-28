7dbe875b414f15936df2dd1e1be1a906da647593
Gianmarco Brocchi <gianmarcobrocchi@gmail.com>
2023-08-28 17:55:46 +0200

    Use rsync back and forth.

--- sync_Plan.sh
+++ sync_Plan.sh
@@ -1,4 +1,4 @@
 # /bin/bash
 # Rsync with Poisson
-echo 'rsync -auv4 "Desktop/PlanTomorrow.md" "brocchi@poisson:~" ' &&  rsync -auv4 "Desktop/PlanTomorrow.md" "brocchi@poisson:~"
+echo 'rsync -auv4 "Desktop/PlanTomorrow.md" "brocchi@poisson:~ and reverse" ' &&  rsync -auv4 --progress "Desktop/PlanTomorrow.md" "brocchi@poisson:~" &&  rsync -auv4 --progress "brocchi@poisson:~/PlanTomorrow.md" "Desktop/" 
 
